3518473ef10bb3246c50b421554e2a0efd9e7f2b
est31 <MTest31@outlook.com>
2019-05-28 23:39:52 +0200

    Add test project

---
+++ test-projects/test01/binary-thing/src/main.rs
@@ -0,0 +1,6 @@
+use library_thing::UsedStruct;
+
+fn main() {
+	let used = UsedStruct;
+	println!("{:?}", used);
+}
---
+++ test-projects/test01/library-thing/src/lib.rs
@@ -0,0 +1,4 @@
+#[derive(Debug)]
+pub struct UsedStruct;
+
+pub struct UnusedStruct;
