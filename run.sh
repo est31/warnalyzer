402fc457fd1be3d5b06173240782dc42a15cd6b7
est31 <MTest31@outlook.com>
2022-09-19 23:21:10 +0200

    Turn run.sh into a proper test runner

--- run.sh
+++ run.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 
-pushd test-projects/test01
-rm -rf target
-RUSTFLAGS="-Z save-analysis" cargo +nightly check
-popd
-cargo run test-projects/test01/target/debug/deps/save-analysis/binary_thing-*.json
+function run_test {
+	pushd test-projects/$1
+	rm -rf target
+	RUSTFLAGS="-Z save-analysis" cargo +nightly check
+	popd
+	cargo run test-projects/$1/target/debug/deps/save-analysis/binary_thing-*.json > test-projects/$1/target/$1.stdout
+	output=$(cat test-projects/$1/target/$1.stdout)
+	expected=$(cat test-projects/$1/$1.stdout)
+	if [ "$expected" != "$output" ]; then
+		echo "Mismatch. Expected:"
+		echo "---------------------"
+		echo "$expected"
+		echo "---------------------"
+		echo "But got:"
+		echo "---------------------"
+		echo "$output"
+		echo "---------------------"
+		exit 1
+	fi
+}
+
+run_test test01
+
+run_test test02
